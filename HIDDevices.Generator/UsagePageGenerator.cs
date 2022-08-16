511826b07c237914c37d7ec986b9ab771e277d8a
Craig Dean <thargy@yahoo.com>
2022-08-16 10:53:11 +0100

    feat: #6 Branched to create new generator
    Added blank generator class.
    Updated solution NuGets.
    Removed submodule 'hid-usage-tables'
    Removed T4 template - however, kept generated code for now to continue compilation and for later comparison.

---
+++ HIDDevices.Generator/UsagePageGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace HIDDevices.Generator
+{
+    public class UsagePageGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // TODO
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // TODO
+        }
+    }
+}
